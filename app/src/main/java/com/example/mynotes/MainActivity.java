d169b957f3354b056143f483fa46e492ed381400
shubhamjain3105 <shubhamjain3199@gmail.com>
2019-09-06 19:26:43 +0530

    Iniitial Commit

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
     HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes",null);
       if(set==null){
           notes.add("Example");
+          //
       }
 
       else
